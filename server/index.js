e493ea31058d9dbd6396888e453d7a3d518c00c9
PiotrZachoszczCodersCrew <piotr.zachoszcz@coderscrew.pl>
2020-09-08 16:10:05 +0200

    running server without https

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
-const https = require("https");
+// const https = require("https");
 const app = express();
-const ssl = require("./utils/ssl");
+// const ssl = require("./utils/ssl");
 
-const httpsServer = https.createServer(ssl, app);
-require("express-ws")(app, httpsServer);
+// const httpsServer = https.createServer(ssl, app);
+// require("express-ws")(app, httpsServer);
+require("express-ws")(app);
 require("./startup/prod")(app);
 require("./startup/db")();
 require("./startup/routes")(app);
 require("./startup/jwt")();
 
 const port = process.env.PORT || 443;
-const index = httpsServer.listen(port, () => {
-  console.log(`HTTPS Server running on port ${port}`);
+// const index = httpsServer.listen(port, () => {
+//   console.log(`HTTPS Server running on port ${port}`);
+// });
+
+app.listen(port, () => {
+  console.log(`
+  Server running on port ${port}`);
 });
