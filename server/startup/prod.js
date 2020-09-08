2e342ce4fd47a7cb7285c27a013f06960748aae8
PiotrZachoszczCodersCrew <piotr.zachoszcz@coderscrew.pl>
2020-09-08 13:07:34 +0200

    router refactoring

--- server/startup/prod.js
+++ server/startup/prod.js
@@ -3,7 +3,6 @@ const compression = require("compression");
 const cors = require("cors");
 
 module.exports = function (app) {
-  app.use();
   app.use(cors({ exposedHeaders: ["x-auth-token"] }));
   app.use(helmet());
   app.use(compression());
