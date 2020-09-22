5a3b4fe681cfd0984162c5841e9d258a4aade3f9
PiotrZachoszczCodersCrew <piotr.zachoszcz@coderscrew.pl>
2020-09-22 17:39:07 +0200

    add header to auth route

--- server/startup/prod.js
+++ server/startup/prod.js
@@ -3,7 +3,7 @@ const compression = require("compression");
 const cors = require("cors");
 
 module.exports = function (app) {
-  app.use(cors({ exposedHeaders: ["x-auth-token"] }));
+  app.use(cors({ exposedHeaders: ["x-auth-token", "is-admin", "is-user"] }));
   app.use(helmet());
   app.use(compression());
 };
